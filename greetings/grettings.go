b8e3ce70254e4deb27ffdb875ac7d869137992a8
Radvil <radvil@@access-mobile.com>
2023-08-07 23:24:01 +0700

    Chapter 3: basic errors handling
    ref >> https://go.dev/doc/tutorial/handle-errors

--- greetings/grettings.go
+++ greetings/grettings.go
@@ -1,8 +1,14 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-  message := fmt.Sprintf("Hello %v, welcome to the jungle!", name)
-  return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("EMPTY_NAME")
+	}
+	message := fmt.Sprintf("Hello %v, welcome to the jungle!", name)
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	firstName := "Radvil"
-	lastName := "Laode"
-	fullName := fmt.Sprintf("%v %v", firstName, lastName)
-	message := greetings.Hello(fullName)
+	// set the prop for the predefined logger
+	// including log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("GREETINGS: ")
+	log.SetFlags(0)
+
+	// request a greeting message.
+	message, err := greetings.Hello("")
+	// if an error was returned, print it to the console
+	// then exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no error was returned print the message instead.
 	fmt.Println(message)
 }
