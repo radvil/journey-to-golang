e0505ef06d75bb562647589913f55037c24280ba
Radvil <radvil@@access-mobile.com>
2023-08-07 23:50:49 +0700

    Chapter 4: Basic object mapping
    ref: https://go.dev/doc/tutorial/greetings-multiple-people

--- greetings/grettings.go
+++ greetings/grettings.go
@@ -3,12 +3,47 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("EMPTY_NAME")
 	}
-	message := fmt.Sprintf("Hello %v, welcome to the jungle!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Hellos returns a map of associates each of the named people
+// with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names and messages
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message to each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with the name
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+// helper function
+// randomFormat returns one of a set of greeting message,
+// the returned message is selected on random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v Welcome!",
+		"Nice to see you, %v",
+		"Hi %v, Well met!",
+	}
+
+	// return on of the random formatted message
+	return formats[rand.Intn(len((formats)))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -14,14 +14,18 @@ func main() {
 	log.SetPrefix("GREETINGS: ")
 	log.SetFlags(0)
 
-	// request a greeting message.
-	message, err := greetings.Hello("")
+	// A slice of names.
+	names := []string{"Traxex", "Injoker", "Nevermore"}
+
+	// request greeting message for the name
+	messages, err := greetings.Hellos(names)
 	// if an error was returned, print it to the console
 	// then exit the program
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// if no error was returned print the message instead.
-	fmt.Println(message)
+	// if no error was returned print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
